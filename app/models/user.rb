3e9e69dcaad23192edbfc832d2f8e39ac9dfb78b
五十嵐太郎 <igarashitaro@MacBookmujirushi.local>
2018-09-17 14:07:24 +0900

    preliminary compeltion of Rails06 without homework

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+    length: { minimum: 1, maximum: 15, allow_blank: true}
+
+  validates :email, presence: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true }
+
+  validates :password, presence: true,
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i, allow_blank: true }
+#    length: { minimum: 8, maximum: 32, allow_blank: true }
+
+  validates :password_confirmation, presence: true,
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i, allow_blank: true }
+#    length: { minimum: 8, maximum: 32, allow_blank: true }
+
+  has_secure_password
+end
