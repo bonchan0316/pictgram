191a7a6d2a14429c3b54a3338766beffd8daff35
五十嵐太郎 <igarashitaro@MacBookmujirushi.local>
2018-09-23 05:58:40 +0900

    preliminary compeltion of Rails06 without homework ver.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, allow_blank: true }
 
   validates :password, presence: true,
-    format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i, allow_blank: true },
-    length: { minimum: 8, maximum: 32, allow_blank: true }
+    format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]{8,32}\z/i, allow_blank: true }#,
+#    length: { minimum: 8, maximum: 32, allow_blank: true }
 
   validates :password_confirmation, presence: true,
-    format: { with: /\A(?=.*[a-z])(?=.*\d)[a-z\d]+\z/i, allow_blank: true },
-    length: { minimum: 8, maximum: 32, allow_blank: true }
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i, allow_blank: true }#,
+#    (?=.*?[a-z])(?=.*?\d)の"()"内の中央の"?"は、省略可能のはず。
+#    length: { minimum: 8, maximum: 32, allow_blank: true }
 
   has_secure_password
 end
